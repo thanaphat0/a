bbb4419b2b3ad83b289948c244db9531642a9570
unknown <you@example.com>
2020-01-12 17:23:01 +0700

    fin

---
+++ app.js
@@ -0,0 +1,28 @@
+const path = require('path');
+
+const bodyParser = require('body-parser');
+const express = require('express');
+
+// const Post = require('./models/post-model');
+const postRoutes = require('./routes/posts-routes');
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
+
+app.get('/', (request, response) => {
+    response.sendFile(path.join(__dirname,'public','index.html'));
+});
+
+app.get('/new',(req,res) => {
+    res.sendFile(path.join(__dirname,'public','edit.html'));
+});
+
+app.use('/posts',postRoutes);
+
+app.listen(3000, () => {
+    console.log('Server is started at http://localhost:3000')
+});
